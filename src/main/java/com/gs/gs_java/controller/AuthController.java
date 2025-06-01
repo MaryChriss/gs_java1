150a72990ec4e5f80932defaacd64ba427acd767
Mariana Christina <161539215+MaryChriss@users.noreply.github.com>
2025-05-31 21:33:23 -0300

    feature: login

---
+++ src/main/java/com/gs/gs_java/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.gs.gs_java.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import  com.gs.gs_java.model.Token;
+import  com.gs.gs_java.model.User;
+import  com.gs.gs_java.service.AuthService;
+import  com.gs.gs_java.service.TokenService;
+
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public Token login(@RequestBody Credentials credentials){ 
+        var user = authService.loadUserByUsername(credentials.email());
+        if (!passwordEncoder.matches(credentials.password(), user.getPassword())){
+            throw new BadCredentialsException("Senha incorreta");
+        }
+        
+        return tokenService.createToken((User) user);
+    }
+    
+}
---
+++ src/main/java/com/gs/gs_java/controller/Credentials.java
@@ -0,0 +1,5 @@
+package com.gs.gs_java.controller;
+
+public record Credentials(String email, String password) {
+
+}
